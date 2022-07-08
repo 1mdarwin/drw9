8282a4343a741646b31ef16de9c7c1eb94bfee6b
Darwin Betancourt <dbetancourt@drwsoluciones.net>
2022-07-08 17:55:18 -0500

    New custom drw2022_barrio theme

---
+++ themes/custom/drw2022_theme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drw2022_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
